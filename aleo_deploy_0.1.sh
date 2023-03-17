c5987201bac4b06bf7ba20b1d4dcb0c0743b5049
appetitoff <87865050+appetitoff@users.noreply.github.com>
2023-03-17 11:10:58 +0200

    Update aleo_deploy_0.1.sh

--- aleo_deploy_0.1.sh
+++ aleo_deploy_0.1.sh
@@ -51,3 +51,5 @@ PATHTOAPP=$(realpath -q $APPNAME)
 echo -e "$PATHTOAPP"
 cd $PATHTOAPP && cd ..
 snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 600000 --record "${RECORD}"
+cd $HOME
+echo -e "Успешный деплой контракта!"
